717d3970c205cd26039e1fc60b478d57507f6880
讖淤 <3209889807@qq.com>
2024-01-24 13:19:33 +0800

    rich去除多余代码

--- src/views/investigation/components/rich-tinymce.vue
+++ src/views/investigation/components/rich-tinymce.vue
@@ -61,7 +61,7 @@ const init = {
     fontsize_formats: '12px 14px 16px 18px 24px 36px 48px 56px 72px',
     font_formats: '微软雅黑=Microsoft YaHei,Helvetica Neue,PingFang SC,sans-serif;苹果苹方=PingFang SC,Microsoft YaHei,sans-serif;宋体=simsun,serif;仿宋体=FangSong,serif;黑体=SimHei,sans-serif;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats;知乎配置=BlinkMacSystemFont, Helvetica Neue, PingFang SC, Microsoft YaHei, Source Han Sans SC, Noto Sans CJK SC, WenQuanYi Micro Hei, sans-serif;小米配置=Helvetica Neue,Helvetica,Arial,Microsoft Yahei,Hiragino Sans GB,Heiti SC,WenQuanYi Micro Hei,sans-serif',
     // 解决粘贴图片后，不自动上传，而是使用base64编码。
-    urlconverter_callback: (url: string, node: any, onSave: any, name: string) => {
+    urlconverter_callback: (url: string) => {
         if (url.startsWith('blob:http')) {
             tinymce.activeEditor && tinymce.activeEditor.uploadImages();
         }
