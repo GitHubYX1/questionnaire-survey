9e6eeeeed15b03e31131bfea9150ae7360c1fdc9
讖淤 <3209889807@qq.com>
2024-03-13 16:34:08 +0800

    数据分析不显示分页

--- src/computed/api.ts
+++ src/computed/api.ts
@@ -1,11 +1,5 @@
 import storage from "@/utils/storage";
-import type {
-  surveyAnswerType,
-  answerType,
-  analysisType,
-  analysisOptionType,
-  fillType,
-} from "@/types/index";
+import type { surveyAnswerType, answerType, analysisType, analysisOptionType, fillType } from "@/types/index";
 import { surveyStore } from "@/stores/survey";
 
 const storeData = surveyStore();
@@ -20,8 +14,7 @@ export async function getAnalysisData(id: string): Promise<{
   end: string;
   data: analysisType[];
 }> {
-  let answerData: surveyAnswerType[] =
-    storage.getSession("ANSWERDATA", id) || [];
+  let answerData: surveyAnswerType[] = storage.getSession("ANSWERDATA", id) || [];
   let survey = storeData.surveySelected(id);
   let answerList: answerType[] = [];
   answerData.forEach((item) => {
@@ -36,45 +29,47 @@ export async function getAnalysisData(id: string): Promise<{
   }
 
   //获取分析数据
-  let data: analysisType[] = survey.question.map((item) => {
-    let answerQuestion = answerList.filter((son) => son.questionId == item.id);
-    let assessCount = answerQuestion.length;
-    let option: analysisOptionType[] = item.option.map((son) => {
-      let count = 0;
-      let assess = item.type !== "多选" ? assessCount : 0;
-      answerQuestion.forEach((answer) => {
-        if (item.type !== "多选") {
-          if (answer.content == son.id) {
-            count += 1;
-          }
-        } else if (answer.content.constructor == Array) {
-          assess += answer.content.length;
-          for (let i in answer.content) {
-            if (answer.content[i] == son.id) {
+  let data: analysisType[] = survey.question
+    .map((item) => {
+      let answerQuestion = answerList.filter((son) => son.questionId == item.id);
+      let assessCount = answerQuestion.length;
+      let option: analysisOptionType[] = item.option.map((son) => {
+        let count = 0;
+        let assess = item.type !== "多选" ? assessCount : 0;
+        answerQuestion.forEach((answer) => {
+          if (item.type !== "多选") {
+            if (answer.content == son.id) {
               count += 1;
             }
+          } else if (answer.content.constructor == Array) {
+            assess += answer.content.length;
+            for (let i in answer.content) {
+              if (answer.content[i] == son.id) {
+                count += 1;
+              }
+            }
           }
-        }
+        });
+        let ratio = count ? Number(((count / assess) * 100).toFixed(2)) : 0;
+        return { ...son, count, ratio };
       });
-      let ratio = count ? Number(((count / assess) * 100).toFixed(2)) : 0;
-      return { ...son, count, ratio };
-    });
-    let fill: fillType[] = [];
-    if (item.type == "填空" && answerQuestion.length) {
-      fill = answerQuestion.map((son, index) => ({
-        xh: index + 1,
-        count: String(son.content),
-      }));
-    }
-    return {
-      id: item.id,
-      title: item.title,
-      type: item.type,
-      option: option,
-      fill: fill,
-      assessCount: assessCount,
-    };
-  });
+      let fill: fillType[] = [];
+      if (item.type == "填空" && answerQuestion.length) {
+        fill = answerQuestion.map((son, index) => ({
+          xh: index + 1,
+          count: String(son.content),
+        }));
+      }
+      return {
+        id: item.id,
+        title: item.title,
+        type: item.type,
+        option: option,
+        fill: fill,
+        assessCount: assessCount,
+      };
+    })
+    .filter((item) => item.type !== "分页");
   return { title: survey.title, count: answerData.length, start, end, data };
 }
 
@@ -82,46 +77,35 @@ export async function getAnalysisData(id: string): Promise<{
  * 答题数据
  * @param id 题目id
  */
-export async function getAnswerData(
-  id: string
-): Promise<{ answer: surveyAnswerType[]; excleList: string[][] }> {
+export async function getAnswerData(id: string): Promise<{ answer: surveyAnswerType[]; excleList: string[][] }> {
   let answer: surveyAnswerType[] = storage.getSession("ANSWERDATA", id) || [];
   let excleTop = ["答卷编号", "开始时间", "结束时间", "耗时"];
   let excleContent: string[][] = [];
   let survey = storeData.surveySelected(id);
   answer.forEach((item, index) => {
-    let answerContent = [
-      item.answerId,
-      item.startTime,
-      item.endTime,
-      item.consumTime,
-    ];
+    let answerContent = [item.answerId, item.startTime, item.endTime, item.consumTime];
     survey.question.forEach((question) => {
-      if (index == 0) {
+      if (index === 0 && question.type !== "分页") {
         excleTop.push(question.id + "." + question.title);
       }
       //获取答题数据
-      let content = item.answer.find(
-        (answer) => answer.questionId == question.id
-      )?.content;
+      let content = item.answer.find((answer) => answer.questionId == question.id)?.content;
       if (content && content.constructor == Array) {
         let textData = [];
         for (let i in content) {
           let id = content[i];
-          let text = question.option.filter((option) => option.id == id)[0]
-            .content;
+          let text = question.option.filter((option) => option.id == id)[0].content;
           textData.push(id + "." + text);
         }
         answerContent.push(textData.join());
       } else if (content) {
         if (question.type === "单选") {
-          let text = question.option.filter((option) => option.id == content)[0]
-            .content;
+          let text = question.option.filter((option) => option.id == content)[0].content;
           answerContent.push(content + "." + text);
         } else {
           answerContent.push(String(content));
         }
-      } else {
+      } else if (question.type !== "分页") {
         answerContent.push("");
       }
     });
