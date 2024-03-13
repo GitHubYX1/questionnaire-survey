6582f1676c7ce4cc790ed0ea381bf6cec3497bb7
讖淤 <3209889807@qq.com>
2024-03-13 09:47:56 +0800

    调整utils;格式化app.vue

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
   <a-config-provider :locale="locale"><RouterView /></a-config-provider>
 </template>
 
-<script lang="ts" setup> 
-  import zhCN from 'ant-design-vue/es/locale/zh_CN';
-  import dayjs from 'dayjs';
-  import 'dayjs/locale/zh-cn';
-  import {RouterView } from 'vue-router'
-  dayjs.locale('zh-cn');
-  let locale = zhCN
+<script lang="ts" setup>
+import zhCN from "ant-design-vue/es/locale/zh_CN";
+import dayjs from "dayjs";
+import "dayjs/locale/zh-cn";
+import { RouterView } from "vue-router";
+dayjs.locale("zh-cn");
+let locale = zhCN;
 </script>
 
 <style scoped>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import dayjs from 'dayjs';
+import * as dayjs from "dayjs";
 import type { optionType } from "@/types/index";
 
 //选项参数初始化
@@ -10,7 +10,7 @@ export function optionInit(): optionType[] {
 }
 
 //评分选项初始化
-export function scoreOptionInit(num:number): optionType[] {
+export function scoreOptionInit(num: number): optionType[] {
   const arr: optionType[] = [
     { id: 1, content: "1星" },
     { id: 2, content: "2星" },
@@ -40,34 +40,29 @@ export function offTime(value: number): string {
   if (!value) return "";
   var date = new Date(value);
   var Y = date.getFullYear() + "-";
-  var M =
-    (date.getMonth() + 1 < 10
-      ? "0" + (date.getMonth() + 1)
-      : date.getMonth() + 1) + "-";
+  var M = (date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1) + "-";
   var D = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
-  var h =
-      (date.getHours() < 10 ? "0" + date.getHours() : date.getHours()) + ":";
-    var m =
-      date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-  return Y + M + D + ' ' + h + m;
+  var h = (date.getHours() < 10 ? "0" + date.getHours() : date.getHours()) + ":";
+  var m = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  return Y + M + D + " " + h + m;
 }
 
 //获取时间
-export function getTime():string {
-  const timeFormat = 'YYYY-MM-DD HH:mm:ss';
+export function getTime(): string {
+  const timeFormat = "YYYY-MM-DD HH:mm:ss";
   return dayjs().format(timeFormat);
 }
 
 //获取时间差
-export function timeDiff(start:string,end:string){
-  let leave1 = new Date(start).getTime()
-  let leave2 = new Date(end).getTime()
-  let leave3= (leave2 - leave1)/ 1000
-  var minute = Math.floor(leave3 / 60)
-  var second = leave3 % 60 
-  if(minute){
-    return `${minute}分${second}秒`
-  }else{
-    return `${second}秒`
+export function timeDiff(start: string, end: string) {
+  let leave1 = new Date(start).getTime();
+  let leave2 = new Date(end).getTime();
+  let leave3 = (leave2 - leave1) / 1000;
+  var minute = Math.floor(leave3 / 60);
+  var second = leave3 % 60;
+  if (minute) {
+    return `${minute}分${second}秒`;
+  } else {
+    return `${second}秒`;
   }
 }
