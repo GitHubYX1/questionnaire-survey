682c1d29ecc4579166899d24394ac21255ae3efc
讖淤 <3209889807@qq.com>
2024-03-15 11:36:28 +0800

    简化富文组件

--- src/views/investigation/components/rich-tinymce.vue
+++ src/views/investigation/components/rich-tinymce.vue
@@ -8,8 +8,7 @@ import { message } from 'ant-design-vue';
 import tinymce from 'tinymce/tinymce' //tinymce默认hidden，不引入不显示
 import Editor from '@tinymce/tinymce-vue'
 import 'tinymce/themes/silver/theme' // 主题文件
-import 'tinymce/icons/default'
-// tinymce插件可按自己的需要进行导入
+import 'tinymce/icons/default'// tinymce插件可按自己的需要进行导入
 import 'tinymce/plugins/image' // 插入上传图片插件
 import 'tinymce/plugins/importcss' //图片工具
 import 'tinymce/plugins/media' // 插入视频插件
@@ -20,13 +19,7 @@ import 'tinymce/plugins/wordcount' // 字数统计插件
 import 'tinymce/plugins/codesample' // 插入代码
 import 'tinymce/plugins/code' // 查看源码
 import 'tinymce/plugins/codesample'; //代码示例
-import 'tinymce/plugins/fullscreen' //全屏
-import 'tinymce/plugins/link' //
-import 'tinymce/plugins/preview' // 预览
-import 'tinymce/plugins/template' //插入模板
-import 'tinymce/plugins/save' // 保存
-import 'tinymce/plugins/searchreplace' //查询替换
-import 'tinymce/plugins/pagebreak' //分页
+import 'tinymce/plugins/link' //编辑链接
 import 'tinymce/plugins/insertdatetime' //时间插入
 import 'tinymce/plugins/nonbreaking' //插入不间断空格
 import 'tinymce/plugins/advlist' //高级列表插件
@@ -54,10 +47,10 @@ const init = {
     language: 'zh-Hans',
     skin: false,
     content_css: false,
-    plugins: 'code preview searchreplace fullscreen image link media template codesample table charmap pagebreak nonbreaking insertdatetime advlist lists wordcount  indent2em',
+    plugins: 'code image link media codesample table charmap nonbreaking insertdatetime advlist lists wordcount indent2em',
     toolbar: 'code undo redo cut copy paste pastetext forecolor backcolor bold italic underline strikethrough link | alignleft aligncenter alignright alignjustify outdent indent indent2em | \
-     styleselect formatselect fontselect fontsizeselect | bullist numlist | blockquote subscript superscript removeformat | \
-                     table image media charmap pagebreak insertdatetime  preview  fullscreen   lineheight ',
+     styleselect formatselect fontselect fontsizeselect | bullist numlist | blockquote subscript superscript removeformat nonbreaking| \
+                     table image media charmap insertdatetime lineheight',
     fontsize_formats: '12px 14px 16px 18px 24px 36px 48px 56px 72px',
     font_formats: '微软雅黑=Microsoft YaHei,Helvetica Neue,PingFang SC,sans-serif;苹果苹方=PingFang SC,Microsoft YaHei,sans-serif;宋体=simsun,serif;仿宋体=FangSong,serif;黑体=SimHei,sans-serif;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats;知乎配置=BlinkMacSystemFont, Helvetica Neue, PingFang SC, Microsoft YaHei, Source Han Sans SC, Noto Sans CJK SC, WenQuanYi Micro Hei, sans-serif;小米配置=Helvetica Neue,Helvetica,Arial,Microsoft Yahei,Hiragino Sans GB,Heiti SC,WenQuanYi Micro Hei,sans-serif',
     // 解决粘贴图片后，不自动上传，而是使用base64编码。
