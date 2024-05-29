9fef4db8faf094ed0367c5b3540a4580e890b9db
讖淤 <3209889807@qq.com>
2024-05-29 14:01:46 +0800

    新增滑动条组件

---
+++ src/components/slider/slider.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="slider-text flex-between">
+    <span>{{ min.content }}</span>
+    <span>{{ max.content }}</span>
+  </div>
+  <a-slider :min="min.id" :max="max.id" @change="sliderChange" :value="value" :disabled="disabled" />
+</template>
+<script lang="ts" setup>
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  value: {
+    type: Number,
+    default: 0
+  },
+  min: {
+    type: Object,
+    default: () => {
+      return {
+        content: "",
+        value: 0
+      }
+    }
+  },
+  max: {
+    type: Object,
+    default: () => {
+      return {
+        content: "",
+        value: 100
+      }
+    }
+  }
+});
+
+const emit = defineEmits(["update:value", "change"]);
+
+const sliderChange = (value: number) => {
+  console.log("value", value);
+  emit("update:value", value);
+  emit("change", value);
+};
+</script>
+<style lang="scss" scoped>
+.slider-text {
+  margin-top: 10px;
+}
+</style>
