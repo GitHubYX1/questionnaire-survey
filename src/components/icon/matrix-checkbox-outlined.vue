864005168c98fc38af44d0f615d1cd62137fcd0a
讖淤 <3209889807@qq.com>
2024-05-30 15:06:31 +0800

    新增矩阵题ui

---
+++ src/components/icon/matrix-checkbox-outlined.vue
@@ -0,0 +1,10 @@
+<template>
+  <span class="anticon">
+    <svg width="1em" height="1em" fill="currentColor" viewBox="0 0 1024 1024" p-id="43088">
+      <path
+        d="M654.6 251.4V21H21v633.6h230.4v345.6h748.8V251.4H654.6zM78.6 597V78.6H597v172.8H251.4V597H78.6z m864 345.6H309V309h633.6v633.6z"
+        p-id="43089"></path>
+      <path d="M836.2 508.1L795 467.9 568.4 699.7 456.8 583.1l-41.6 39.8 152.7 159.6z" p-id="43090"></path>
+    </svg>
+  </span>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import "ant-design-vue/dist/antd.css";
 import { loading } from "@/components/loading/loading";
 import sliderOutlined from "@/components/icon/slider-outlined.vue";
 import radioOutlined from "@/components/icon/radio-outlined.vue";
+import matrixCheckboxOutlined from "@/components/icon/matrix-checkbox-outlined.vue";
+import matrixRadioOutlined from "@/components/icon/matrix-radio-outlined.vue";
+import matrixSliderOutlined from "@/components/icon/matrix-slider-outlined.vue";
 import "./assets/css/main.css";
 
 import App from "./App.vue";
@@ -18,4 +21,7 @@ for (const i in iconData) {
 }
 app.component("slider-outlined", sliderOutlined);
 app.component("radio-outlined", radioOutlined);
+app.component("matrix-checkbox-outlined", matrixCheckboxOutlined);
+app.component("matrix-radio-outlined", matrixRadioOutlined);
+app.component("matrix-slider-outlined", matrixSliderOutlined);
 app.use(createPinia()).use(router).use(design).provide("loading", loading).mount("#app");
